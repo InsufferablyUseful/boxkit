79c0cd0385ac08a8964380ca42fd339290e2d212
Ben Robertson <benrobertson150@hotmail.co.uk>
2026-05-13 16:39:54 +0100

    Add script to export useful binaries so they can be used without distrobox enter

---
+++ scripts/alpineexport.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+distrobox-export --bin /usr/bin/elinks
+distrobox-export --bin /usr/bin/newsboat
+distrobox-export --bin /usr/bin/chezmoi
+distrobox-export --bin /usr/bin/youtube-tui
+distrobox-export --bin /usr/bin/tmux
+distrobox-export --bin /usr/bin/nvim
+distrobox-export --bin /usr/bin/btop
+distrobox-export --bin /usr/bin/vimdiff
+distrobox-export --bin /usr/bin/cosign
+distrobox-export --bin /usr/bin/yazi
+distrobox-export --bin /usr/bin/vncviewer
+distrobox-export --bin /usr/bin/mpv
+distrobox-export --bin /usr/bin/chafa
+distrobox-export --bin /usr/bin/fbi
+distrobox-export --bin /usr/bin/fbgs
+distrobox-export --bin /usr/bin/zathura
+distrobox-export --bin /usr/bin/imv
+distrobox-export --bin /usr/bin/alpine
+distrobox-export --bin /usr/bin/elinks
+distrobox-export --bin /usr/bin/lynx
+distrobox-export --bin /usr/bin/yt-dlp
+distrobox-export --bin /usr/bin/bombadillo
+distrobox-export --bin /usr/bin/newsboat
