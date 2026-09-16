67132788d46f718569f8172d81fbfe0e93b41a01
Ben Robertson <benrobertson150@hotmail.co.uk>
2026-09-16 22:54:33 +0100

    Add new box for graphical apps on alpine.

---
+++ scripts/alpine-apps-export.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+distrobox-export --app darktable
+distrobox-export --app pinta
+distrobox-export --app vlc
+distrobox-export --app transmission
+distrobox-export --app papers
+distrobox-export --app zotero
+distrobox-export --app weather
