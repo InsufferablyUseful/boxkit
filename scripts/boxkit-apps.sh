713e2f95e045c45b6a8abd0719f6450136619400
InsufferablyUseful <131925450+InsufferablyUseful@users.noreply.github.com>
2026-09-17 00:23:37 +0100

    Add new box for graphical apps on alpine. (#18)

---
+++ scripts/boxkit-apps.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Symlink distrobox shims
+./distrobox-shims.sh
+
+#Add testing repository
+#echo "@testing http://dl-cdn.alpinelinux.org/alpine/edge/testing" >> /etc/apk/repositories
+
+# Update the container and install packages
+apk update && apk upgrade
+
+grep -v '^#' ./boxkit-apps.packages | xargs apk add
+#Install bitwarden-cli
