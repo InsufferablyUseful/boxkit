c7e9870c04be11ce2c398ae4e1e5d86722bb4d1c
InsufferablyUseful <131925450+InsufferablyUseful@users.noreply.github.com>
2026-09-22 00:25:09 +0100

    Add yt-x. (#21)
    * Add yt-x.
    
    * put yt-x in root /bin.

--- scripts/boxkit.sh
+++ scripts/boxkit.sh
@@ -12,4 +12,9 @@ apk update && apk upgrade
 grep -v '^#' ./boxkit.packages | xargs apk add
 #Install bitwarden-cli
 npm install -g @bitwarden/cli
+#Install visidata
 pipx install --global visidata
+#Install yt-x
+curl -sL "https://github.com/Benexl/yt-x/releases/download/v0.8.6/yt-x" -o /bin/yt-x
+chmod +x /bin/yt-x
+
